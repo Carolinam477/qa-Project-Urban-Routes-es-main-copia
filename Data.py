c537b6fc1792f30a76e1b477cbd87584c4194417
Carolinam477 <Carolinacelis477@gmail.com>
2024-09-13 22:40:57 +0200

    Data.py
    urban_routes_url = 'https://cnt-6a427187-e1e6-4b37-91b7-20bc7abdcb0d.containerhub.tripleten-services.com?lng=es'
    address_from = 'East 2nd Street, 601'
    address_to = '1300 1st St'
    phone_number = '+1 123 123 12 12'
    card_number, card_code = '1234 5678 9100', '111'
    message_for_driver = 'Trae un aperitivo'

---
+++ Data.py
@@ -0,0 +1,6 @@
+urban_routes_url = 'https://cnt-6a427187-e1e6-4b37-91b7-20bc7abdcb0d.containerhub.tripleten-services.com?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Trae un aperitivo'
